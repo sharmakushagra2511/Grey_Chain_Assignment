98bbdd8738fa4082d80c08e314457bccd8b70ea4
sharmakushagra.2511 <sharmakushagra.2511@gmail.com>
2023-09-19 02:17:43 +0530

    Dev Initial commit

--- assignment.loanmanagement/build.gradle.kts
+++ assignment.loanmanagement/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 	mavenCentral()
 }
 
+//dependencies
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
