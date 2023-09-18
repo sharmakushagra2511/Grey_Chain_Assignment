497013e66c34a212ec6be16562ce72701cf56d32
sharmakushagra.2511 <sharmakushagra.2511@gmail.com>
2023-09-19 02:11:29 +0530

    Initial Commit

---
+++ assignment.loanmanagement/src/test/kotlin/com/assignment/loanmanagement/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.assignment.loanmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
